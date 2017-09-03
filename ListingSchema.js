f65710f9f23009a0f724ec61673a37c8b9002149
Your Name <anthonybrugal@yahoo.com>
2017-09-03 17:16:14 -0400

    Final for assignment 3

--- ListingSchema.js
+++ ListingSchema.js
@@ -1,14 +1,34 @@
 /* Import mongoose and define any variables needed to create the schema */
-var mongoose = require('mongoose'), 
+var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 /* Create your schema */
 var listingSchema = new Schema({
+  code: {type: String, required: true},
+  name: {type: String, required: true},
+  coordinates:
+  {
+    latitude: Number,
+    longitude: Number
+  },
+  address: String,
+  created_at: Date,
+  updated_at: Date
+
   /* your code here */
+
 });
 
 /* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
 listingSchema.pre('save', function(next) {
+  var currentDate = new Date();
+
+  this.updated_at = currentDate;
+
+  if (!this.created_at)
+    this.created_at = currentDate;
+
+  next();
   /* your code here */
 });
 
--- queries.js
+++ queries.js
@@ -1,28 +1,113 @@
 /* Fill out these functions using Mongoose queries*/
 
-var findLibraryWest = function() {
-  /* 
+var Listing = require('./ListingSchema.js'),
+mongoose = require('mongoose'),
+config = require('./config');
+
+mongoose.connect(config.db.uri);
+
+var findLibraryWest = function()
+{
+  /*
     Find the document that contains data corresponding to Library West,
-    then log it to the console. 
+    then log it to the console.
    */
+   Listing.findOne({'name' : 'Library West'}, function(err, listings)
+   {
+     if(err) throw err;
+     console.log("");
+     console.log("");
+     console.log("");
+     console.log("");
+     console.log("");
+
+     console.log(listings);
+     console.log("");
+     console.log("");
+     console.log("");
+     console.log("");
+     console.log("");
+   });
 };
-var removeCable = function() {
+
+
+
+var removeCable = function()
+{
   /*
-    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
+    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
-    and remove this listing from your database and log the document to the console. 
+    and remove this listing from your database and log the document to the console.
    */
-};
-var updatePhelpsMemorial = function() {
+  Listing.findOneAndRemove({'code': 'CABL'}, function(err, listings, res)
+  {
+    if (err) throw err;
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("");
+
+    console.log(listings);
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("");
+
+  });
+ };
+
+
+
+var updatePhelpsMemorial = function()
+{
   /*
-    Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then 
-    log the updated document to the console. 
+    Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then
+    log the updated document to the console.
    */
+   Listing.findOneAndUpdate({'name': 'Phelps Laboratory'}, {$set: {'address': 'Gainesville, FL 32603'}}, function(err, listings)
+   {
+      if (err) throw err;
+      console.log("");
+      console.log("");
+      console.log("");
+      console.log("");
+      console.log("");
+
+      console.log(listings);
+      console.log("");
+      console.log("");
+      console.log("");
+      console.log("");
+      console.log("");
+
+   });
 };
-var retrieveAllListings = function() {
-  /* 
-    Retrieve all listings in the database, and log them to the console. 
+
+
+var retrieveAllListings = function()
+{
+  /*
+    Retrieve all listings in the database, and log them to the console.
    */
+   Listing.find({}, function(err, listings)
+   {
+    if (err) throw err;
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("");
+
+    console.log(listings);
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("");
+
+   });
 };
 
 findLibraryWest();
